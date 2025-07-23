35324cd485ba10d673f6c8ad12729c13f03ac35b
bdjang <bdjang@gmail.com>
2025-07-22 20:51:43 -0400

    Remove pixel art component related code

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,12 @@ import Button from './components/Button'
 import PixelArt from './components/PixelArt'
 
 export default function App() {
-    
-    const asianManEl = document.getElementsByClassName("asianMan");
-    const flameArtEl = document.getElementsByClassName("flameArt");
-    const magicalSwordEl = document.getElementsByClassName("magicalSword");
-    const pixelPanelEl = document.getElementsByClassName("pixelPanel");
-    const sunGlassesEl = document.getElementsByClassName("sunGlasses");
-    function pixelClick() {
-        magicalSwordEl[0].style.opacity = '1';
-        magicalSwordEl[0].style.transform = 'translate(0) rotate(360deg)';
-        setTimeout(() => { magicalSwordEl[0].children[0].style.filter = 'invert(100%)'; }, 1500);
-        sunGlassesEl[0].style.opacity = '1';
-        sunGlassesEl[0].style.transform = 'translate(0)';
-    }
-    function pixelLeave() {
-        magicalSwordEl[0].style.opacity = '0';
-        magicalSwordEl[0].style.transform = 'translate(-400px, -500px) rotate(0)';
-        magicalSwordEl[0].children[0].style.filter = 'invert(0%)';
-        sunGlassesEl[0].style.opacity = '0';
-        sunGlassesEl[0].style.transform = 'translate(0, -350px)';
-        pixelPanelEl[0].style.filter = '';
-    }
-    function pixelShadow() {
-        const ranBoxShadow1 = "#" + (Math.random() * 0xffffff << 0).toString(16).padStart(6, "0");
-        pixelPanelEl[0].style.filter = 'drop-shadow(0 0 2em ' + ranBoxShadow1 + ')';
-    }
-
     return (
         <>
             <div className="titleText">
                 <h1 onMouseEnter={e => e.target.style.color = '#f2f2f2'} onMouseLeave={e => e.target.style.color = '#f2f2f2'}>bdjang.github.io</h1>
             </div>
-            <div className="pixelPanel" onClick={pixelClick} onMouseOver={pixelShadow} onMouseLeave={pixelLeave} onTouchEnd={pixelLeave}>
-                <PixelArt />
-            </div>
+            <PixelArt />
             <div className="introText">
                 <h2>I am a web tinkerer. I experiment with code. I sometimes create digital art.</h2>
             </div>
