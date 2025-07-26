05f4e37d61dc5858c590e647c98469de320107ff
bdjang <bdjang@gmail.com>
2025-07-25 21:31:08 -0400

    Import CSS from project module, add div wrapper element

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,12 @@
+import { projectDesc as projectDescClass } from "./Project.module.css"
+
 export default function Project(props) {
     return (
         <>
-            <h2>{props.projectTitle}</h2>
-            <p>{props.projectDescription}</p>
+            <div className={projectDescClass}>
+                <h2>{props.projectTitle}</h2>
+                <p>{props.projectDescription}</p>
+            </div>
         </>
     )
 }
