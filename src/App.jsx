18b58d6a3b83a9be903cd88976fea3807e93e758
bdjang <bdjang@gmail.com>
2025-08-05 20:45:17 -0400

    Update projectTitle prop values for the Project components

--- src/App.jsx
+++ src/App.jsx
@@ -19,17 +19,17 @@ export default function App() {
                 <h2>I am a web tinkerer. I experiment with code. I sometimes create digital art.</h2>
             </div>
             <hr />
-            <Project projectTitle="Current Working Project: 🌗 Dark Mode Pixel Art" projectDescription={ // Pass the link as a prop and compose the description in JSX
+            <Project projectTitle="Dark Mode Pixel Art" projectDescription={ // Pass the link as a prop and compose the description in JSX
                 <>
                     Using the inbox as my canvas, I create code-based pixel art that displays one way in light mode and another in dark mode. The pixel art adjusts based on the email client's settings in dark mode.
                 </>
             } />
-            <Project projectTitle="Past Project: 📊 Email Chart Builder" projectDescription={
+            <Project projectTitle="Email Chart Builder" projectDescription={
                 <>
                     A tool for building bar charts for HTML email templates. <a href="https://charts.email/" target="_blank" rel="noopener noreferrer">Visit project site</a>
                 </>
             } />
-            <Project projectTitle="Past Project: 📜 Email Pixel Alphabet" projectDescription={
+            <Project projectTitle="Email Pixel Alphabet" projectDescription={
                 <>
                     A tool for creating pixelated letters for emails or digital art. <a href="https://github.com/bdjang/" target="_blank" rel="noopener noreferrer">Visit project repo</a>
                 </>
