037adf9fc87dd23907eade496205e3bb58f56c21
bdjang <bdjang@gmail.com>
2025-08-27 20:53:34 -0400

    Create LoadingIndicator component

---
+++ src/components/LoadingIndicator.tsx
@@ -0,0 +1,9 @@
+type LoadingIndicatorProps = {
+    loadingState: string;
+};
+
+const LoadingIndicator = ({ loadingState }: LoadingIndicatorProps) => {
+    return <h2>{loadingState}</h2>;
+};
+
+export default LoadingIndicator;
