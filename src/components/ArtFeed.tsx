b2638528b1bc98a3a45d6b55e5f86f538409de66
bdjang <bdjang@gmail.com>
2025-08-27 20:56:59 -0400

    Import loadingIndicator and loadingStatus components, display loadingStatus within fetch function

--- src/components/ArtFeed.tsx
+++ src/components/ArtFeed.tsx
@@ -1,24 +1,37 @@
 import { useEffect, useState } from 'react';
 import styles from './ArtFeed.module.scss';
 import DOMPurify from 'dompurify';
+import LoadingIndicator from './LoadingIndicator.js';
+import loadingStatus from '../helpers/loadingStatus.js'
 
 function useFetchData(url: string) {
     const [data, setData] = useState([]);
+    const [loadingState, setLoadingState] = useState(loadingStatus.isLoading);
+    
+    // .then/.catch is promised based
+    // try/catch with async/await is more readable and easier to handle multiple async operations
     useEffect(() => {
+        setLoadingState(loadingStatus.isLoading);
         fetch(url)
             .then(res => res.json())
-            .then(json => setData(json));
+            .then(json => { setData(json); setLoadingState(loadingStatus.loaded); })
+            .catch(() => setLoadingState(loadingStatus.hasErrored));
     }, [url]);
-    return data;
+    return { data, loadingState };
 }
 
+
 export default function ArtFeed() {
-    const data = useFetchData('https://eg-collection-server-89ee5d23663f.herokuapp.com/');
+    const { data, loadingState } = useFetchData('https://eg-collection-server-89ee5d23663f.herokuapp.com/');
     return (
-        <ul className={styles.artItems}>
-            {data.map((item: any) => (
-                <li key={item._id} title={item.createdAt} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(item.pixelCode) }} />
-            ))}
-        </ul>
+        <section className={styles.artItems}>
+            <ul>
+                {loadingState === loadingStatus.isLoading ? <LoadingIndicator loadingState={loadingState} /> : '' } {/* This is a conditional rendering expression in React */}
+                {/* === is the strict equality operator. It checks that both value and type are the same. */}
+                {data.map((item: any) => (
+                    <li key={item._id} title={item.createdAt} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(item.pixelCode) }} />
+                ))}
+            </ul>
+        </section>
     );
 }
