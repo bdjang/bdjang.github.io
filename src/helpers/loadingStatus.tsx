fb3b4110d845b26979657fe2cfdf995a6e7c63e5
bdjang <bdjang@gmail.com>
2025-08-27 20:53:15 -0400

    Create helpers folder, create loadingStatus React helper

---
+++ src/helpers/loadingStatus.tsx
@@ -0,0 +1,9 @@
+// React helpers are utility functions, custom hooks, or small components that assist with common tasks in React apps
+
+const loadingStatus = {
+    loaded: 'Loaded',
+    isLoading: 'Loading...',
+    hasErrored: 'An error occurred while loading',
+};
+
+export default loadingStatus;
