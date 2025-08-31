e8d266cd3e3ea4e67774a7dc1f3266bacc506a9b
bdjang <bdjang@gmail.com>
2025-08-31 19:57:13 -0400

    Add conditional rendering to add comingSoon class

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,4 @@
-import styles from "./Project.module.scss"
+import styles from './Project.module.scss'
 
 interface projectProps { // Used to define types for objects, functions, and other structures in TypeScript
     projectTitle: React.ReactNode; // Type for text, JSX, or components; maximum flexibility
@@ -9,7 +9,7 @@ export default function Project(props: projectProps) {
     return (
         <>
             <article className={styles.projectDesc}>
-                <h2>{props.projectTitle}</h2>
+                <h2 className={typeof props.projectTitle === 'string' && props.projectTitle.includes('Email') ? styles.comingSoon : ''}>{props.projectTitle}</h2> {/* Using conditional rendering to include an h2 class */}
                 <p>{props.projectDescription}</p>
             </article>
         </>
