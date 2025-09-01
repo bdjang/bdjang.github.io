9d0132e1aa89e8e3e1cb2d9371b3dda4b339ceb0
bdjang <bdjang@gmail.com>
2025-09-01 17:07:27 -0400

    Add global TypeScript definition for .mp4 files

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,9 @@
 declare module '*.scss';
-declare module "*.module.scss" {
+declare module '*.module.scss' {
     const classes: { [key: string]: string };
     export default classes;
+}
+declare module '*.mp4' {
+    const src: string;
+    export default src;
 }
