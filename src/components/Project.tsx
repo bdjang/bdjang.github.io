099deba4288ea060daaf572a90da4fbc074a39ac
bdjang <bdjang@gmail.com>
2025-09-02 21:01:03 -0400

    Destructure React props, pass in src prop, update article element click logic

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,16 +1,19 @@
 import styles from './Project.module.scss'
+import VideoModal from './VideoModal.js';
 
 interface projectProps { // Used to define types for objects, functions, and other structures in TypeScript
     projectTitle: React.ReactNode; // Type for text, JSX, or components; maximum flexibility
     projectDescription: React.ReactNode;
+    selectProject: any;
+    src: string;
 }
 
-export default function Project(props: projectProps) {
+export default function Project({ projectTitle, projectDescription, selectProject, src }: projectProps) {
     return (
         <>
-            <article className={styles.projectDesc}>
-                <h2 className={typeof props.projectTitle === 'string' && props.projectTitle.includes('Email') ? styles.comingSoon : ''}>{props.projectTitle}</h2> {/* Using conditional rendering to include an h2 class */}
-                <p>{props.projectDescription}</p>
+            <article onClick={() => { selectProject(<VideoModal src={src} />) }} className={styles.projectDesc}>
+                <h2 className={typeof projectTitle === 'string' && projectTitle.includes('Email') ? styles.comingSoon : ''}>{projectTitle}</h2> {/* Using conditional rendering to include an h2 class */}
+                <p>{projectDescription}</p>
             </article>
         </>
     )
