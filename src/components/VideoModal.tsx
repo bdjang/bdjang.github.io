22e11e7249114a8d38c8f358d445206f0989994e
bdjang <bdjang@gmail.com>
2025-09-02 20:59:38 -0400

    Pass in src prop to VideoModal component, remove import statements for .mp4 files

--- src/components/VideoModal.tsx
+++ src/components/VideoModal.tsx
@@ -1,12 +1,14 @@
 import styles from './VideoModal.module.scss';
-import egTutorial from '../videos/email-graffiti-tutorial.mp4';
-import deaTutorial from '../videos/djang-email-art-tutorial.mp4';
 
-export default function VideoModal() {
+interface videoProps {
+    src: string;
+}
+
+export default function VideoModal({ src }: videoProps) { // Update VideoModal to accept src as prop. Then go up the component chain and keep adding the src prop all the way to the App.tsx file. You have to chain this prop all the way to the top of the App.tsx which uses it in the Project component
     return (
         <>
             <section className={styles.modalVideo}>
-                <video src={egTutorial} width="470" height="540" controls></video><a>Close</a>
+                <video src={src} width="470" height="540" controls></video><a>Close</a>
             </section>
         </>
     )
