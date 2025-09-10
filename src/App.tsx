c3a11f3a4d1caf19749337f32f4db98f51aa97da
bdjang <bdjang@gmail.com>
2025-09-09 20:23:33 -0400

    Add VideoModal and placeholderVideo import statements, create ternary operator statement to render VideoModal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss'
 import { useState } from 'react';
-import PixelArt from './components/PixelArt.js'
-import Project from './components/Project.js'
-import PixelName from './components/PixelName.js'
-import Footer from './components/Footer.js'
-import ArtFeed from './components/ArtFeed.js'
+import PixelArt from './components/PixelArt.js';
+import Project from './components/Project.js';
+import PixelName from './components/PixelName.js';
+import Footer from './components/Footer.js';
+import ArtFeed from './components/ArtFeed.js';
+import VideoModal from './components/VideoModal.js';
+import placeholderVideo from './videos/placeholder-video.mp4';
 
 export default function App() {
     // Initialize ranColorOne and ranColorTwo React state variables, pass current values as props to PixelName and PixelArt components
@@ -12,6 +14,7 @@ export default function App() {
     const ranColorTwo = "#" + (Math.random() * 0xffffff << 0).toString(16).padStart(6, "0");
     const [pixelColorOne, setPixelColorOne] = useState(ranColorOne);
     const [pixelColorTwo, setPixelColorTwo] = useState(ranColorTwo);
+    const [selectedProject, setSelectedProject] = useState<string | undefined>();
 
     return (
         <>
@@ -22,21 +25,39 @@ export default function App() {
             </section>
             <hr />
             <section>
-                <Project projectTitle="Dark Mode Pixel Art" projectDescription={ // Pass the link as a prop and compose the description in JSX
-                    <>
-                        Using the inbox as my canvas, I create code-based pixel art that displays one way in light mode and another in dark mode. The pixel art adjusts based on the email client's settings in dark mode.
-                    </>
-                } />
-                <Project projectTitle="Email Chart Builder" projectDescription={
-                    <>
-                        A tool for building bar charts for HTML email templates. <a href="https://charts.email/" target="_blank" rel="noopener noreferrer">Visit project site</a>
-                    </>
-                } />
-                <Project projectTitle="Email Pixel Alphabet" projectDescription={
-                    <>
-                        A tool for creating pixelated letters for emails or digital art. <a href="https://github.com/bdjang/" target="_blank" rel="noopener noreferrer">Visit project repo</a>
-                    </>
-                } />
+                {selectedProject ? <VideoModal src={selectedProject} setSelectedProject={setSelectedProject} /> :
+                    <Project
+                        src={placeholderVideo}
+                        selectProject={() => setSelectedProject(placeholderVideo)}
+                        projectTitle="Charts.Email"
+                        projectDescription={ // Pass the link as a prop and compose the description in JSX
+                        <>
+                            A tool for building bar charts for HTML email templates. <a href="https://charts.email/" target="_blank" rel="noopener noreferrer">Visit project site</a>
+                        </>
+                    } />
+                }
+                {selectedProject ? <VideoModal src={selectedProject} setSelectedProject={setSelectedProject} /> :
+                    <Project
+                        src={placeholderVideo}
+                        selectProject={() => setSelectedProject(placeholderVideo)}
+                        projectTitle="Djang Email Art"
+                        projectDescription={
+                        <>
+                            A site for managing and showcasing my personal digital art collection.
+                        </>
+                    } />
+                }
+                {selectedProject ? <VideoModal src={selectedProject} setSelectedProject={setSelectedProject} /> :
+                    <Project
+                        src={placeholderVideo}
+                        selectProject={() => setSelectedProject(placeholderVideo)}
+                        projectTitle="Email Graffiti"
+                        projectDescription={
+                        <>
+                            A digital art tool for creating freehand art that can be placed into email quickly and spontaneously.
+                        </>
+                    } />
+                }
             </section>
             <ArtFeed />
             <Footer />
