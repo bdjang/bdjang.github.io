dde61982c32d1a1bc19266d1fc7d5cf407daeb9b
bdjang <bdjang@gmail.com>
2025-09-13 16:20:27 -0400

    Add onKeyDown escape event handler, add tabIndex property to section element

--- src/components/VideoModal.tsx
+++ src/components/VideoModal.tsx
@@ -8,7 +8,14 @@ interface videoProps {
 export default function VideoModal({ src, setSelectedProject }: videoProps) { // Update VideoModal to accept src as prop. Then go up the component chain and keep adding the src prop all the way to the App.tsx file. You have to chain this prop all the way to the top of the App.tsx which uses it in the Project component
     return (
         <>
-            <section className={styles.modalVideo}>
+            <section
+                tabIndex={0}
+                className={styles.modalVideo}
+                onKeyDown={(e) => {
+                    if (e.key == 'Escape') {
+                        setSelectedProject(null);
+                    }
+                }}>
                 <a onClick={() => setSelectedProject(null)}>Close</a> {/* React event handler that calls setSelectedProject function with null as argument and resets selected project state in parent component */}
                 {typeof src === 'string' && src.includes('graffiti') ? <video src={src} width="478" height="540" controls></video> : <video src={src} width="750" height="500" controls></video>}
             </section>
