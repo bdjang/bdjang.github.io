a3faa9c88109bf04ac741de34c50f51325fb7cdd
bdjang <bdjang@gmail.com>
2025-12-08 19:26:05 -0500

    Comment out onClick project video effect

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -12,7 +12,7 @@ export default function Project({ projectTitle, projectDescription, selectProjec
         <>
             <article className={styles.projectDesc}>
                 {typeof projectTitle === 'string' && (projectTitle.includes('Djang') || projectTitle.includes('Graffiti')) ? <span className={styles.comingSoon}>Coming soon</span> : ''}
-                <h2 onClick={() => selectProject(src) }>{projectTitle}</h2> {/* Using conditional rendering to include an h2 class. Pass only the video source (src) to selectProject, not the React element. Let the parent component handle rendering <VideoModal /> based on the selected video. */}
+                <h2 /* commented out for now, uncomment when new videos are set; onClick={() => selectProject(src) } */>{projectTitle}</h2> {/* Using conditional rendering to include an h2 class. Pass only the video source (src) to selectProject, not the React element. Let the parent component handle rendering <VideoModal /> based on the selected video. */}
                 {/* 9.3.25: You have to keep adding the individual component properties (props) whenever they are used in other components */}
                 <p>{projectDescription}</p>
             </article>
